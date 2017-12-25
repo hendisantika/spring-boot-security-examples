9030a1ee90c6a4fa58ab49939b7245062a5bf348
Hendi Santika <hendisantika@gmail.com>
2017-12-26 06:47:01 +0700

    #13 - Modify Main App

---
+++ spring-boot-actuator-security/src/main/java/com/hendisantika/config/Application.java
@@ -0,0 +1,24 @@
+package com.hendisantika.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-actuator-security
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/12/17
+ * Time: 06.23
+ * To change this template use File | Settings | File Templates.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
