74f0d079b8e520bba494e2791c5fcf7bb0be397e
Hendi Santika <hendisantika@gmail.com>
2017-12-25 07:06:54 +0700

    #4 - Modify Main App

--- spring-security-example-jsp/src/main/java/com/hendisantika/config/Application.java
+++ spring-security-example-jsp/src/main/java/com/hendisantika/config/Application.java
@@ -5,6 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/12/17
+ * Time: 06.08
+ * To change this template use File | Settings | File Templates.
+ */
+
 //This is a convenience annotation that is equivalent to 
 //declaring @Configuration, @EnableAutoConfiguration and @ComponentScan.
 @ComponentScan(basePackages = "com.hendisantika")
