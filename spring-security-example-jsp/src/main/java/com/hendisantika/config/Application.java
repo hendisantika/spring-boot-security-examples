12f03ee24dfc78fc41a069dfebc14648e7899fa7
Hendi Santika <hendisantika@gmail.com>
2017-12-25 07:05:12 +0700

    #3 - Add Main Application

---
+++ spring-security-example-jsp/src/main/java/com/hendisantika/config/Application.java
@@ -0,0 +1,18 @@
+package com.hendisantika.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+//This is a convenience annotation that is equivalent to 
+//declaring @Configuration, @EnableAutoConfiguration and @ComponentScan.
+@ComponentScan(basePackages = "com.hendisantika")
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
