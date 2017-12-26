4338f70801f74d7c58583b8a090836f06a28a447
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:36:55 +0700

    #19 - Add pom.xml - spring-security-basic-auth

---
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hendisantika.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private AuthenticationEntryPoint authEntryPoint;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic()
+				.authenticationEntryPoint(authEntryPoint);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
+	}
+
+}
