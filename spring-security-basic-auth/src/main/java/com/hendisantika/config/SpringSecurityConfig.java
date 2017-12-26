22d85fb5d45c4dd7e3c06b14874872196beabd01
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:41:31 +0700

    #23 - Add Spring Security Config

--- spring-security-basic-auth/src/main/java/com/hendisantika/config/SpringSecurityConfig.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/SpringSecurityConfig.java
@@ -7,24 +7,36 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-basic-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.26
+ * To change this template use File | Settings | File Templates.
+ */
+
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
-	private AuthenticationEntryPoint authEntryPoint;
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests()
-				.anyRequest().authenticated()
-				.and().httpBasic()
-				.authenticationEntryPoint(authEntryPoint);
-	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
-	}
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("john123").password("password").roles("USER");
+    }
 
 }
