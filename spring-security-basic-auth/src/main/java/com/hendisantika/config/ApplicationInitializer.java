9b1e0e4211dde0e83783544006555e46805c3427
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:40:10 +0700

    #21 - Add Application Initializer

--- spring-security-basic-auth/src/main/java/com/hendisantika/config/ApplicationInitializer.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/ApplicationInitializer.java
@@ -1,16 +1,27 @@
 package com.hendisantika.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-basic-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.18
+ * To change this template use File | Settings | File Templates.
+ */
+
+
 public class ApplicationInitializer implements WebApplicationInitializer {
 
-    public void onStartup(ServletContext container) throws ServletException {
+    public void onStartup(ServletContext container) {
 
         AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
         ctx.setServletContext(container);
