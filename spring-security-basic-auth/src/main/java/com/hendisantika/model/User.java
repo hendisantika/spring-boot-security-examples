da682245787cbb25c4c4795fb857aa72c2ef1cc2
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:42:10 +0700

    #24 - Add User Entity

--- spring-security-basic-auth/src/main/java/com/hendisantika/model/User.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/model/User.java
@@ -1,19 +1,31 @@
 package com.hendisantika.model;
 
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-basic-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.25
+ * To change this template use File | Settings | File Templates.
+ */
+
+
 public class User {
 
-	private String id;
-	private String name;
+    private String id;
+    private String name;
     private String address;
     private String email;
-    
-    public String getId() {
-		return id;
-	}
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
