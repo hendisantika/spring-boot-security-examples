318872e61d01133ef6fb3510520c895fe66db1d3
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:40:55 +0700

    #22 - Add Authentication Entry Point

--- spring-security-basic-auth/src/main/java/com/hendisantika/config/AuthenticationEntryPoint.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/AuthenticationEntryPoint.java
@@ -1,31 +1,41 @@
 package com.hendisantika.config;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-basic-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.22
+ * To change this template use File | Settings | File Templates.
+ */
+
+
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
 
-	@Override
+    @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-      throws IOException, ServletException {
+            throws IOException {
         response.addHeader("WWW-Authenticate", "Basic realm=" +getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         PrintWriter writer = response.getWriter();
         writer.println("HTTP Status 401 - " + authEx.getMessage());
     }
-	
-	@Override
+
+    @Override
     public void afterPropertiesSet() throws Exception {
-        setRealmName("DeveloperStack");
+        setRealmName("Developer Stack");
         super.afterPropertiesSet();
     }
 
