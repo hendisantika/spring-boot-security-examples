f4c0644a6f4bfa2b50ceb4422d00ef10833cc648
Hendi Santika <hendisantika@gmail.com>
2017-12-27 06:39:38 +0700

    #20 - Modify Main Application

--- spring-security-basic-auth/src/main/java/com/hendisantika/config/Application.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/Application.java
@@ -2,12 +2,12 @@ package com.hendisantika.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.hendisantika")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer{
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
