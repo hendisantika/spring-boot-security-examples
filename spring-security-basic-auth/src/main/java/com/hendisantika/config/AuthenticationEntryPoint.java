7ab0348eed1493c2b508ab9eaed8832d13cec3d5
Hendi Santika <hendisantika@gmail.com>
2017-12-27 17:06:27 +0700

    #34 - Add Cross Origin Request

--- spring-security-basic-auth/src/main/java/com/hendisantika/config/AuthenticationEntryPoint.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/config/AuthenticationEntryPoint.java
@@ -1,8 +1,12 @@
 package com.hendisantika.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +27,17 @@ import java.io.PrintWriter;
 
 @Component
 public class AuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
