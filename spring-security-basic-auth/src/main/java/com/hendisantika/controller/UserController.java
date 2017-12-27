2c449e5f35bb05788831b43877067c96a744b11c
Hendi Santika <hendisantika@gmail.com>
2017-12-27 17:05:16 +0700

    #33 - Add logout API

--- spring-security-basic-auth/src/main/java/com/hendisantika/controller/UserController.java
+++ spring-security-basic-auth/src/main/java/com/hendisantika/controller/UserController.java
@@ -3,12 +3,17 @@ package com.hendisantika.controller;
 import com.hendisantika.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 
@@ -68,6 +73,15 @@ public class UserController {
         user3.setAddress("Konohagakure, Japan");
 		return Arrays.asList(user, user1, user2, user3);
 	}
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
+    }
 
 
 }
