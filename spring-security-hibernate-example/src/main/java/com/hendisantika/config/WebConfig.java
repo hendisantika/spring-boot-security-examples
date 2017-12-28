70b9e072ba48690d5568eb4677221a407506ef55
Hendi Santika <hendisantika@gmail.com>
2017-12-28 21:08:11 +0700

    #43 - Add Web Config in Spring Security Hibernate Example

---
+++ spring-security-hibernate-example/src/main/java/com/hendisantika/config/WebConfig.java
@@ -0,0 +1,47 @@
+package com.hendisantika.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-hibernate-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.32
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/*.js/**").addResourceLocations("/ui/static/");
+        registry.addResourceHandler("/*.css/**").addResourceLocations("/ui/static/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/ui/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+}
