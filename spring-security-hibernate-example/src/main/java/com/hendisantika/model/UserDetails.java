ad9494f20e2f5033eadf4be7126cb045b6e71217
Hendi Santika <hendisantika@gmail.com>
2017-12-28 21:09:17 +0700

    #44 - Add User Details Entity in Spring Security Hibernate Example

---
+++ spring-security-hibernate-example/src/main/java/com/hendisantika/model/UserDetails.java
@@ -0,0 +1,77 @@
+package com.hendisantika.model;
+
+import javax.persistence.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-hibernate-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/12/17
+ * Time: 06.34
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Entity
+@Table
+public class UserDetails {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String password;
+
+    @Column
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
