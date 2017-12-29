a916cc19cd05810d340c9e1dccfbb7f155675ccf
Hendi Santika <hendisantika@gmail.com>
2017-12-30 05:21:58 +0700

    #54 - Add data.sql script for data initialization files in Spring Security Hibernate Example

---
+++ spring-security-hibernate-example/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','naruto@example.com','Uzumaki Naruto','naruto');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','sasuke@example.com','Uchiha Sasuke','sasuke');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','sakura@example.com','Haruno Sakura','sakura');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','kakashi@example.com','Hateka Kakashi','kakashi');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','neji@example.com','Hyuuga Neji','neji');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','hinata@example.com','Hyuuga Hinata','hinata');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','shikamaru@example.com','Nara Shikamaru','shikamaru');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','chouji@example.com','Akimichi Chouji','chouji');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','ino@example.com','Yamanaka Ino','ino');
+INSERT INTO userdetails(address,email,name,password)VALUES ('Konohagakure,Japan','kiba@example.com','Inuzuka Kiba','kiba');
