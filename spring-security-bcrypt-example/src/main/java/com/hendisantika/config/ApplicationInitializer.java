b7727192395f8f0eb020c93e31e6336c031a7114
Hendi Santika <hendisantika@gmail.com>
2018-01-03 05:45:43 +0700

    #68 - Add Application Initializer in Spring Security BCrypt Example

---
+++ spring-security-bcrypt-example/src/main/java/com/hendisantika/config/ApplicationInitializer.java
@@ -0,0 +1,31 @@
+package com.hendisantika.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-bcrypt-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/02/18
+ * Time: 05.50
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    public void onStartup(ServletContext container) {
+
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.setServletContext(container);
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+}
