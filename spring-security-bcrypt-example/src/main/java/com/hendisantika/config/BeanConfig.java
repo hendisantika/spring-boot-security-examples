4b07a69a21d681775ef0eb516461ed77948c7e5b
Hendi Santika <hendisantika@gmail.com>
2018-01-03 05:47:48 +0700

    #70 - Add Bean Config in Spring Security BCrypt Example

---
+++ spring-security-bcrypt-example/src/main/java/com/hendisantika/config/BeanConfig.java
@@ -0,0 +1,59 @@
+package com.hendisantika.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-security-bcrypt-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/02/18
+ * Time: 05.56
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.hendisantika")
+public class BeanConfig {
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("com.hendisantika.model");
+        sessionFactory.setHibernateProperties(hibernateProperties());
+        return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/BCrypt?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    Properties hibernateProperties() {
+        return new Properties() {
+            {
+                setProperty("hibernate.hbm2ddl.auto", "create");
+                setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
+                setProperty("hibernate.show_sql", "true");
+            }
+        };
+    }
+
+}
